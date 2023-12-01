aae69052a76c50f331f166968c0d336a4cce2c7d
idcomcn <idcomcn2003@163.com>
2023-12-01 17:14:02 +0800

    修改代码

--- 2023-week-49/app/src/main/java/com/czh/myapplication/MainActivity.java
+++ 2023-week-49/app/src/main/java/com/czh/myapplication/MainActivity.java
@@ -1,11 +1,12 @@
 package com.czh.myapplication;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,22 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.e("idcomcn,", "onclick: toolbar被点击了");
+                Toast.makeText(MainActivity.this, "toolbar被点击了", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
 
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.add_item:
-                Toast.makeText(this, "臭小子你按到添加啦哈哈哈", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.remove_item:
-                Toast.makeText(this, "你按删除干什么呀!", Toast.LENGTH_SHORT).show();
-                break;
-        }
-        return true;
-    }
 }
