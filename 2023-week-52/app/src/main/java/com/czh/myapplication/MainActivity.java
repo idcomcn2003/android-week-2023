62273d90f7ecd485a13b1dad3791f5db31922b50
idcomcn <idcomcn2003@163.com>
2024-01-06 14:30:37 +0800

    init

--- 2023-week-52/app/src/main/java/com/czh/myapplication/MainActivity.java
+++ 2023-week-52/app/src/main/java/com/czh/myapplication/MainActivity.java
@@ -1,43 +1,54 @@
 package com.czh.myapplication;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.text_view);
+        addListenerRadioGroup();
     }
 
-    public void onClickShow(View view) {
-        //创建一个构造器
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("温馨提示")
-                .setMessage("确定要退出登录吗?")
-                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Log.e("idcomcn", "onClick: 点击了取消");
-                        Toast.makeText(MainActivity.this, "取消", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Log.e("idcomcn", "onClick: 点击了确定");
-                        Toast.makeText(MainActivity.this, "确定", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .create()
-                .show();
+    private void addListenerRadioGroup() {
+        RadioGroup radioGroup = findViewById(R.id.radio_group);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                String city = "";
+                // 处理选项的选择事件
+                switch (checkedId) {
+                    case R.id.radio_button1:
+                        // 选中了Option 1
+                        city = "北京";
+                        break;
+                    case R.id.radio_button2:
+                        // 选中了Option 2
+                        city = "上海";
+                        break;
+                    case R.id.radio_button3:
+                        // 选中了Option 3
+                        city = "广州";
+                        break;
+                    case R.id.radio_button4:
+                        // 选中了Option 3
+                        city = "深圳";
+                        break;
+                    default:
+                        break;
+                }
+                textView.setText("您选择的城市是:" + city);
+            }
+        });
     }
 
 
