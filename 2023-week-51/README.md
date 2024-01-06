da684b3ab207e73309fceee060005aba5a02d706
idcomcn <idcomcn2003@163.com>
2024-01-06 13:38:47 +0800

    init

--- 2023-week-51/README.md
+++ 2023-week-51/README.md
@@ -3,4 +3,4 @@
 ## 开发工具-android-studio-v3.5
 
 这是一个android 2023年的每周一例-第51周的例子
-一个简单notification例子
+一个简单AlertDialog例子
--- 2023-week-51/app/src/main/java/com/czh/myapplication/MainActivity.java
+++ 2023-week-51/app/src/main/java/com/czh/myapplication/MainActivity.java
@@ -1,7 +1,11 @@
 package com.czh.myapplication;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,7 +18,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickShow(View view) {
-
+        //创建一个构造器
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("温馨提示")
+                .setMessage("确定要退出登录吗?")
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.e("idcomcn", "onClick: 点击了取消");
+                        Toast.makeText(MainActivity.this, "取消", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.e("idcomcn", "onClick: 点击了确定");
+                        Toast.makeText(MainActivity.this, "确定", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .create()
+                .show();
     }
 
 
