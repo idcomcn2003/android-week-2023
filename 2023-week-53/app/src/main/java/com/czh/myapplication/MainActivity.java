937a31c0bde651a1e7b71edd0f73ae156de55a65
idcomcn <idcomcn2003@163.com>
2024-01-06 15:14:31 +0800

    init

--- 2023-week-53/app/src/main/java/com/czh/myapplication/MainActivity.java
+++ 2023-week-53/app/src/main/java/com/czh/myapplication/MainActivity.java
@@ -1,13 +1,15 @@
 package com.czh.myapplication;
 
 import android.os.Bundle;
-import android.widget.RadioGroup;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SeekBar seekBar;
     private TextView textView;
 
     @Override
@@ -15,38 +17,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.text_view);
-        addListenerRadioGroup();
+        bindViews();
     }
 
-    private void addListenerRadioGroup() {
-        RadioGroup radioGroup = findViewById(R.id.radio_group);
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+    private void bindViews() {
+        textView = findViewById(R.id.text_view);
+        seekBar = findViewById(R.id.seek_bar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
             @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                String city = "";
-                // 处理选项的选择事件
-                switch (checkedId) {
-                    case R.id.radio_button1:
-                        // 选中了Option 1
-                        city = "北京";
-                        break;
-                    case R.id.radio_button2:
-                        // 选中了Option 2
-                        city = "上海";
-                        break;
-                    case R.id.radio_button3:
-                        // 选中了Option 3
-                        city = "广州";
-                        break;
-                    case R.id.radio_button4:
-                        // 选中了Option 3
-                        city = "深圳";
-                        break;
-                    default:
-                        break;
-                }
-                textView.setText("您选择的城市是:" + city);
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView.setText("当前进度值:" + progress + " /100");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(MainActivity.this, "触碰SeekBar", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(MainActivity.this, "放开SeekBar", Toast.LENGTH_SHORT).show();
             }
         });
     }
